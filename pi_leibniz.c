68d3e27a9ae207384be8219f09f4004161743b65
chiehen <hce880123@gmail.com>
2021-04-27 00:48:53 +0800

    Add pi_leibniz to compare different formula

---
+++ pi_leibniz.c
@@ -0,0 +1,55 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "tpool.h"
+
+#define PRECISION 150000 /* upper bound in BPP sum */
+
+/* Use Bailey–Borwein–Plouffe formula to approximate PI */
+static void *bpp(void *arg)
+{
+    int k = *(int *) arg;
+    double sum = (4.0 / (8 * k + 1)) - (2.0 / (8 * k + 4)) -
+                 (1.0 / (8 * k + 5)) - (1.0 / (8 * k + 6));
+    double *product = malloc(sizeof(double));
+    if (product)
+        *product = 1 / pow(16, k) * sum;
+    return (void *) product;
+}
+
+/* Use Gregory-Leibniz series to approximate PI */
+static void *gls(void *arg)
+{
+    int k = *(int *) arg;
+    double *product = malloc(sizeof(double));
+    if (product) {
+        double temp  = (k & 1) ? (-1) : 1;
+        *product = temp / (2 * k + 1) * 4;
+    }
+    return product;
+}
+
+int main()
+{
+    int bpp_args[PRECISION + 1];
+    double bpp_sum = 0;
+    tpool_t pool = tpool_create(4);
+    tpool_future_t futures[PRECISION + 1];
+
+    for (int i = 0; i <= PRECISION; i++) {
+        bpp_args[i] = i;
+        futures[i] = tpool_apply(pool, gls, (void *) &bpp_args[i]);
+    }
+
+    for (int i = 0; i <= PRECISION; i++) {
+        double *result = tpool_future_get(futures[i], 5 /* blocking wait */);
+        if(result) bpp_sum += *result;
+        tpool_future_destroy(futures[i]);
+        free(result);
+    }
+
+    tpool_join(pool);
+    printf("PI calculated with %d terms: %.15f\n", PRECISION + 1, bpp_sum);
+    return 0;
+}
